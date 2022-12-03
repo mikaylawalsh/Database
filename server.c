1fdb2b50f04eb91a2dc6f926a89fc1de99df32ee
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-03 14:19:39 -0500

    wip

--- server.c
+++ server.c
@@ -95,6 +95,8 @@ void client_constructor(FILE *cxstr) {
     // to the input argument.
     // Step 2: Create the new client thread running the run_client routine.
     // Step 3: Detach the new client thread
+
+    fprintf(stderr, "here");
     
     client_t *c;
     if ((c = (client_t *) malloc(sizeof(client_t))) == NULL) {
