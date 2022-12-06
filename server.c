0b3f5c7d00cbf25e51b06d1e4617a618222f4f85
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-06 16:39:29 -0500

    wip

--- server.c
+++ server.c
@@ -338,9 +338,8 @@ int main(int argc, char *argv[]) {
         int r = read(0, buffer, MAX);
         if (r > 0) {
             char *bufz;
-            char file[512];
+            char *file;
             char temp[512];
-            memset(file, 0, 512);
             memset(temp, 0, 512);
             strcpy(temp, &buffer[1]);
             file = strtok(buffer, "\t\n ");
