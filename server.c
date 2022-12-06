c5136eb4a45a08c2d9ff39f65685c39e6d01587a
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-06 11:24:18 -0500

    wip

--- server.c
+++ server.c
@@ -337,7 +337,7 @@ int main(int argc, char *argv[]) {
             } else if (strcmp(&buffer[0], "p")) {
                 //print
                 char file[512];
-                sscanf(&buffer[1], " %s", file); //error check
+                sscanf(&buffer[1], "%s", file); //error check
                 fprintf(stderr, "%s", file);
                 if (file != NULL) {
                     db_print(file);
