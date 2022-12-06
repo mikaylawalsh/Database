0bebb97ad5bcf10ac206bd49b4724ad035aedba2
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-06 13:16:53 -0500

    wip

--- server.c
+++ server.c
@@ -329,7 +329,7 @@ int main(int argc, char *argv[]) {
         int r = read(0, buffer, MAX);
         if (r > 0) {
             char bufz[1]; 
-            bufz[0] = &buffer[0];
+            bufz[0] = buffer[0];
             if (strcmp(bufz, "s")) { //all matching into here? 
                 client_control_stop();
                 printf("stopped");
