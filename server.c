f3f392f3085c958f2a0749d62e017a19f6573b8f
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-06 16:28:14 -0500

    wip

--- server.c
+++ server.c
@@ -338,8 +338,8 @@ int main(int argc, char *argv[]) {
         int r = read(0, buffer, MAX);
         if (r > 0) {
             char *bufz;
-            bufz = strtok(buffer, "\t\n ");
             printf("%s", &buffer[1]);
+            bufz = strtok(buffer, "\t\n ");
             if (!strcmp(bufz, "s")) { 
                 printf("stopped\n");
                 client_control_stop();
