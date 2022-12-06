e82031a2e179015115bd7a305c79738673332f21
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-06 13:15:14 -0500

    wip

--- server.c
+++ server.c
@@ -328,7 +328,8 @@ int main(int argc, char *argv[]) {
         memset(buffer, 0, MAX);
         int r = read(0, buffer, MAX);
         if (r > 0) {
-            char bufz[] = &buffer[0];
+            char bufz[]; 
+            bufz = &buffer[0];
             if (strcmp(bufz, "s")) { //all matching into here? 
                 client_control_stop();
                 printf("stopped");
