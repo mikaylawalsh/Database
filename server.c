806d64db2fadd63200654c9d91697c23a7b1cf50
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-07 14:59:44 -0500

    wip

--- server.c
+++ server.c
@@ -354,6 +354,7 @@ int main(int argc, char *argv[]) {
     char buffer[MAX];
     memset(buffer, 0, MAX);
     while (read(0, buffer, MAX) > 0) {
+        printf("%s", buffer);
         if (buffer != NULL) {
             char bufz[1];
             char file[512];
