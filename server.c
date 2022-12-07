9ea73f1e561247d83ad5c6895b72b3d2ae73c632
mikaylawalsh <mikayla_walsh@brown.edu>
2022-12-07 09:22:59 -0500

    wip

--- server.c
+++ server.c
@@ -278,16 +278,17 @@ void *monitor_signal(void *arg) {
     set = (sigset_t *) arg;
     int sig;
     while(1) {
-        if (scontrol.num_client_threads == 0) {
-            printf(" sigint received. no clients to terminate.\n");
-            return NULL;
-        }
         if (sigwait(set, &sig) == 0) {
             if (sig == SIGINT) {
-                printf(" sigint received. terminating all clients.\n");
-                pthread_mutex_lock(&thread_list_mutex);
-                delete_all();
-                pthread_mutex_unlock(&thread_list_mutex);
+                if (scontrol.num_client_threads == 0) {
+                    printf(" sigint received. no clients to terminate.\n");
+                    return NULL;
+                } else {
+                    printf(" sigint received. terminating all clients.\n");
+                    pthread_mutex_lock(&thread_list_mutex);
+                    delete_all();
+                    pthread_mutex_unlock(&thread_list_mutex);
+                }
             }
         } else { 
             //error check
